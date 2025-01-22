151f0d5af09d4dc555153cff214d400d7872195c
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-01-22 15:15:52 -0700

    Try with absolute

--- web/src/main/java/org/springframework/security/web/servlet/util/matcher/ServletRequestMatcherBuilders.java
+++ web/src/main/java/org/springframework/security/web/servlet/util/matcher/ServletRequestMatcherBuilders.java
@@ -16,18 +16,6 @@
 
 package org.springframework.security.web.servlet.util.matcher;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletRegistration;
-import jakarta.servlet.http.HttpServletRequest;
-
-import org.springframework.http.HttpMethod;
-import org.springframework.security.web.servlet.util.matcher.ServletRegistrationsSupport.RegistrationMapping;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcherBuilder;
 import org.springframework.util.Assert;
@@ -55,8 +43,6 @@ import org.springframework.util.Assert;
  */
 public final class ServletRequestMatcherBuilders {
 
-	private static final RequestMatcher DEFAULT_SERVLET = ServletRequestMatcher.defaultServlet();
-
 	private ServletRequestMatcherBuilders() {
 	}
 
@@ -67,7 +53,7 @@ public final class ServletRequestMatcherBuilders {
 	 * default servlet
 	 */
 	public static RequestMatcherBuilder defaultServlet() {
-		return servletPathInternal(DEFAULT_SERVLET);
+		return servletPathInternal("");
 	}
 
 	/**
@@ -83,108 +69,20 @@ public final class ServletRequestMatcherBuilders {
 	 * given servlet path
 	 */
 	public static RequestMatcherBuilder servletPath(String servletPath) {
-		return servletPathInternal(new ServletRequestMatcher(servletPath));
+		Assert.notNull(servletPath, "servletPath cannot be null");
+		Assert.isTrue(servletPath.startsWith("/"), "servletPath must start with '/'");
+		Assert.isTrue(!servletPath.endsWith("/"), "servletPath must not end with '/'");
+		Assert.isTrue(!servletPath.endsWith("/*"), "servletPath must not end with '/*'");
+		return servletPathInternal(servletPath);
 	}
 
-	private static RequestMatcherBuilder servletPathInternal(RequestMatcher servletPath) {
+	private static RequestMatcherBuilder servletPathInternal(String servletPath) {
 		PathPatternRequestMatcher.Builder builder = PathPatternRequestMatcher.builder();
 		return (method, pattern) -> {
 			Assert.notNull(pattern, "pattern cannot be null");
 			Assert.isTrue(pattern.startsWith("/"), "pattern must start with '/'");
-			return new AndRequestMatcher(servletPath, builder.pattern(method, pattern));
+			return builder.pattern(method, servletPath + pattern);
 		};
-	}
-
-	/**
-	 * Create {@link RequestMatcher}s that will deduce the servlet path by inspecting
-	 * {@link ServletRegistration}s at runtime
-	 * @return a {@link ServletRequestMatcherBuilders} that deduces the servlet path at
-	 * request time
-	 */
-	public static RequestMatcherBuilder servletPathDeducing() {
-		return (method, pattern) -> {
-			Assert.notNull(pattern, "pattern cannot be null");
-			Assert.isTrue(pattern.startsWith("/"), "pattern must start with '/'");
-			return new PathDeducingRequestMatcher(method, pattern);
-		};
-	}
-
-	static final class PathDeducingRequestMatcher implements RequestMatcher {
-
-		private final Map<ServletContext, RequestMatcher> delegates = new ConcurrentHashMap<>();
-
-		private final HttpMethod method;
-
-		private final String pattern;
-
-		PathDeducingRequestMatcher(HttpMethod method, String pattern) {
-			this.method = method;
-			this.pattern = pattern;
-		}
-
-		RequestMatcher requestMatcher(HttpServletRequest request) {
-			return this.delegates.computeIfAbsent(request.getServletContext(), (servletContext) -> {
-				PathPatternRequestMatcher path = PathPatternRequestMatcher.builder().pattern(this.method, this.pattern);
-				ServletRegistrationsSupport registrations = new ServletRegistrationsSupport(servletContext);
-				Collection<RegistrationMapping> mappings = registrations.mappings();
-				if (mappings.isEmpty()) {
-					return path;
-				}
-				Collection<RegistrationMapping> dispatcherServletMappings = registrations.dispatcherServletMappings();
-				if (dispatcherServletMappings.isEmpty()) {
-					return path;
-				}
-				if (dispatcherServletMappings.size() > 1) {
-					String errorMessage = computeErrorMessage(servletContext.getServletRegistrations().values());
-					throw new IllegalArgumentException(errorMessage);
-				}
-				RegistrationMapping dispatcherServlet = dispatcherServletMappings.iterator().next();
-				if (mappings.size() > 1 && !dispatcherServlet.isDefault()) {
-					String errorMessage = computeErrorMessage(servletContext.getServletRegistrations().values());
-					throw new IllegalArgumentException(errorMessage);
-				}
-				if (dispatcherServlet.isDefault()) {
-					return path;
-				}
-				else {
-					String mapping = dispatcherServlet.mapping();
-					return new AndRequestMatcher(
-							ServletRequestMatcher.servletPath(mapping.substring(0, mapping.length() - 2)), path);
-				}
-			});
-		}
-
-		@Override
-		public boolean matches(HttpServletRequest request) {
-			return matcher(request).isMatch();
-		}
-
-		@Override
-		public MatchResult matcher(HttpServletRequest request) {
-			return requestMatcher(request).matcher(request);
-		}
-
-		private static String computeErrorMessage(Collection<? extends ServletRegistration> registrations) {
-			String template = """
-					This method cannot decide whether these patterns are Spring MVC patterns or not. \
-					This is because there is more than one mappable servlet in your servlet context: %s.
-
-					To address this, please create one ServletRequestMatcherBuilder#servletPath for each servlet that has \
-					authorized endpoints and use them to construct request matchers manually. \
-					If all your URIs are unambiguous, then you can simply publish one ServletRequestMatcherBuilders#servletPath as \
-					a @Bean and Spring Security will use it for all URIs""";
-			Map<String, Collection<String>> mappings = new LinkedHashMap<>();
-			for (ServletRegistration registration : registrations) {
-				mappings.put(registration.getClassName(), registration.getMappings());
-			}
-			return String.format(template, mappings);
-		}
-
-		@Override
-		public String toString() {
-			return "PathDeducingRequestMatcher [delegates = " + this.delegates + "]";
-		}
-
 	}
 
 }
