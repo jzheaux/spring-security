5f8a68be8c7bf326467eb2e0601e69d41fc9765d
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-05-28 17:42:28 -0600

    Allow Default Ordering for TargetVisitor
    In tests, we want to both test that functionality works and also
    demonstrate common or expected usage, where possible. It is likely
    incorrect to use @Order(0) for a target visitor as this states that
    it should take precedence over all Spring Security visitors defined
    at a lower precedence.
    
    Also, it appears this may have been added this way because of a mock
    visitor that appears to be unused by any tests. Further, when an
    application has multiple visitors, they should use the TargetVisitor.of
    method to publish one bean with the order determined by the order
    of the method parameters instead of having two separate beans.
    
    This commit removes the @Order(0) annotation and also the mock
    visitor, deferring to the natural ordering afforded by the
    framework.
    
    Issue gh-15994

--- config/src/test/java/org/springframework/security/config/annotation/method/configuration/PrePostMethodSecurityConfigurationTests.java
+++ config/src/test/java/org/springframework/security/config/annotation/method/configuration/PrePostMethodSecurityConfigurationTests.java
@@ -40,7 +40,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mockito;
 
 import org.springframework.aop.Advisor;
 import org.springframework.aop.Pointcut;
@@ -63,7 +62,6 @@ import org.springframework.context.annotation.Role;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.core.annotation.AnnotationConfigurationException;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
@@ -1762,14 +1760,6 @@ public class PrePostMethodSecurityConfigurationTests {
 
 		@Bean
 		@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-		@Order(1)
-		static TargetVisitor mock() {
-			return Mockito.mock(TargetVisitor.class);
-		}
-
-		@Bean
-		@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-		@Order(0)
 		static TargetVisitor skipValueTypes() {
 			return TargetVisitor.defaultsSkipValueTypes();
 		}
